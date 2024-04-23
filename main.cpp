757637b578051798bdf9e5dc811095bbe51e74de
vb01 <cyb3rcr4sh2000@gmail.com>
2024-04-23 17:29:04 -0400

    Created the main program.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/* #include <iostream>
+ * #include <ctime>
+ * void display_time(const time_t* tim)
+ * {
+ * 	std::cout << "The time is: " << std::localtime(tim);
+ * };
+ *
+ * void display_time(struct tm* tim)
+ * {
+ * 	std::cout << "The time is: " << std::mktime(tim);
+ * };
+ *
+ * int main()
+ * {
+ * 	std::time_t tim = std::mktime(0);
+ * 	struct std::tm* ltim = std::localtime(&tim);
+ * 	display_time(ltim);
+ * 	display_time(&tim);
+ * 	return 0;
+ * }
+ */
+
+
