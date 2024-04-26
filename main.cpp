3f0d157a34e8ae1864699eaacd7426e9fa1495e6
vb01 <cyb3rcr4sh2000@gmail.com>
2024-04-25 20:02:48 -0400

    I looked up an example for this program and it worked. Now I am just improvising the code to get similar results. After compiling I get errors messages or a segmentation fault.
    I added a NOTES.txt file to take notes as I go.

--- main.cpp
+++ main.cpp
@@ -2,20 +2,21 @@
 #include <ctime>
 
 // Display Time Function
-void DisplayTime(const time_t* tim);
+//void DisplayTime(const time_t* tim);
        
 // The main() function
 int main()
 {
-	std::time_t tim = std::asctime(0);
-	std::tm* ltim = std::localtime(&tim);
-	DisplayTime(*ltim);
+	std::time_t tim = mktime(0);
+	std::tm *ltim = localtime(&tim);
+
+	std::cout << "Today is " << &ltim << std::endl;
 
 	return 0;
 }
 
-void DisplayTime(const time_t* tim)
-{
-	std::cout << "The time is: " << std::localtime(tim);
-}
+//void DisplayTime(const time_t* tim)
+//{
+//	std::cout << "The time is: " << std::localtime(tim);
+//}
 
