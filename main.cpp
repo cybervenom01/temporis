ec26ea303544678c796edea9ff02bc1ddf21f3ba
vb01 <cyb3rcr4sh2000@gmail.com>
2024-04-26 20:16:04 -0400

    Tried modifying the main file. I need to be creative when it comes to coding.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 #include <ctime>
 
-// Display Time Function
-//void DisplayTime(const time_t* tim);
-       
 // The main() function
 int main()
 {
-	std::time_t tim = mktime(0);
-	std::tm *ltim = localtime(&tim);
+	std::time_t tim = std::time(nullptr);
+	//std::tm* ltim = std::localtime(&tim);
 
-	std::cout << "Today is " << &ltim << std::endl;
+	std::cout << std::ctime(&tim) << std::endl;
+
+	//std::cout << (ltim->tm_year + 1900) << '-' << (ltim->tm_mon + 1) << '-' << (ltim->tm_mday) << std::endl;
+
+	//std::cout << (ltim->tm_hour) << ':' << (ltim->tm_min) << ':' << (ltim->tm_sec) << std::endl;
 
 	return 0;
 }
-
-//void DisplayTime(const time_t* tim)
-//{
-//	std::cout << "The time is: " << std::localtime(tim);
-//}
-
