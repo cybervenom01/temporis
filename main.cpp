5f7e14e46459849c396ac7c16a3530ceb6a499a3
vb01 <cyb3rcr4sh2000@gmail.com>
2024-05-01 17:00:46 -0400

    Made a few edits and improvements on the main program.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,11 @@
 // The main() function
 int main()
 {
-	std::time_t tim = std::time(nullptr);
-	//std::tm* ltim = std::localtime(&tim);
+	std::time_t tim = time(0);
+	struct std::tm* ltim = gmtime(&tim);
 
-	std::cout << std::ctime(&tim) << std::endl;
-
-	//std::cout << (ltim->tm_year + 1900) << '-' << (ltim->tm_mon + 1) << '-' << (ltim->tm_mday) << std::endl;
-
-	//std::cout << (ltim->tm_hour) << ':' << (ltim->tm_min) << ':' << (ltim->tm_sec) << std::endl;
+	std::cout << std::asctime(ltim) << std::endl;	// XXX: Returns correct time and date
+	std::cout << std::ctime(&tim) << std::endl;	// FIXME: Returns a hexadecimal number instead of time and date
 
 	return 0;
 }
